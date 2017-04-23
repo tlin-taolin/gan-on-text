16fcc3782b5e5ef25230898d8c6c16395c340372
Tao Lin <lintaog3@gmail.com>
2017-04-23 15:16:18 +0200

    update.

---
+++ code/test/platform/hdf5/config.lua
@@ -0,0 +1,4 @@
+hdf5._config = {
+    HDF5_INCLUDE_PATH = "/usr/local/hdf5/include",
+    HDF5_LIBRARIES = "/usr/local/lib/libhdf5.dylib;/usr/local/lib/libsz.dylib;/usr/lib/libz.dylib;/usr/lib/libdl.dylib;/usr/lib/libm.dylib"
+}
