6fed2844a191f532b9a464b2abcef1ccba3af29b
Tao Lin <lintaog3@gmail.com>
2017-04-24 22:59:53 +0200

    update.

--- code/test/torch-gan/lfw_adverserial.lua
+++ code/test/torch-gan/lfw_adverserial.lua
@@ -111,159 +111,161 @@ function adversarial.train(dataset, N)
   local dataBatchSize = opt.batchSize / 2
   local time = sys.clock()
 
-  -- do one epoch
-  print('\n<trainer> on training set:')
-  print("<trainer> online epoch # " .. epoch .. ' [batchSize = ' .. opt.batchSize .. ' lr = ' .. sgdState_D.learningRate .. ', momentum = ' .. sgdState_D.momentum .. ']')
-  for t = 1,N,dataBatchSize do
+  while epoch <=2 do
+    -- do one epoch
+    print('\n<trainer> on training set:')
+    print("<trainer> online epoch # " .. epoch .. ' [batchSize = ' .. opt.batchSize .. ' lr = ' .. sgdState_D.learningRate .. ', momentum = ' .. sgdState_D.momentum .. ']')
+    for t = 1,N,dataBatchSize do
 
-    local inputs = torch.Tensor(opt.batchSize, opt.geometry[1], opt.geometry[2], opt.geometry[3])
-    local targets = torch.Tensor(opt.batchSize)
-    local noise_inputs = torch.Tensor(opt.batchSize, opt.noiseDim)
+      local inputs = torch.Tensor(opt.batchSize, opt.geometry[1], opt.geometry[2], opt.geometry[3])
+      local targets = torch.Tensor(opt.batchSize)
+      local noise_inputs = torch.Tensor(opt.batchSize, opt.noiseDim)
 
-    ----------------------------------------------------------------------
-    -- create closure to evaluate f(X) and df/dX of discriminator
-    local fevalD = function(x)
-      collectgarbage()
-      if x ~= parameters_D then -- get new parameters
-        parameters_D:copy(x)
+      ----------------------------------------------------------------------
+      -- create closure to evaluate f(X) and df/dX of discriminator
+      local fevalD = function(x)
+        collectgarbage()
+        if x ~= parameters_D then -- get new parameters
+          parameters_D:copy(x)
+        end
+
+        gradParameters_D:zero() -- reset gradients
+
+        --  forward pass
+        local outputs = model_D:forward(inputs)
+       -- err_F = criterion:forward(outputs:narrow(1, 1, opt.batchSize / 2), targets:narrow(1, 1, opt.batchSize / 2))
+       -- err_R = criterion:forward(outputs:narrow(1, (opt.batchSize / 2) + 1, opt.batchSize / 2), targets:narrow(1, (opt.batchSize / 2) + 1, opt.batchSize / 2))
+       err_R = criterion:forward(outputs:narrow(1, 1, opt.batchSize / 2), targets:narrow(1, 1, opt.batchSize / 2))
+       err_F = criterion:forward(outputs:narrow(1, (opt.batchSize / 2) + 1, opt.batchSize / 2), targets:narrow(1, (opt.batchSize / 2) + 1, opt.batchSize / 2))
+
+      local margin = 0.3
+        sgdState_D.optimize = true
+        sgdState_G.optimize = true
+        if err_F < margin or err_R < margin then
+           sgdState_D.optimize = false
+        end
+        if err_F > (1.0-margin) or err_R > (1.0-margin) then
+           sgdState_G.optimize = false
+        end
+        if sgdState_G.optimize == false and sgdState_D.optimize == false then
+           sgdState_G.optimize = true
+           sgdState_D.optimize = true
+        end
+
+
+        --print(monA:size(), tarA:size())
+        io.write("v1_lfw| R:", err_R,"  F:", err_F, "  ")
+        local f = criterion:forward(outputs, targets)
+
+        -- backward pass
+        local df_do = criterion:backward(outputs, targets)
+        model_D:backward(inputs, df_do)
+
+        -- penalties (L1 and L2):
+        if opt.coefL1 ~= 0 or opt.coefL2 ~= 0 then
+          local norm,sign= torch.norm,torch.sign
+          -- Loss:
+          f = f + opt.coefL1 * norm(parameters_D,1)
+          f = f + opt.coefL2 * norm(parameters_D,2)^2/2
+          -- Gradients:
+          gradParameters_D:add( sign(parameters_D):mul(opt.coefL1) + parameters_D:clone():mul(opt.coefL2) )
+        end
+        -- update confusion (add 1 since targets are binary)
+        for i = 1,opt.batchSize do
+          local c
+          if outputs[i][1] > 0.5 then c = 2 else c = 1 end
+          confusion:add(c, targets[i]+1)
+        end
+        --print('grad D', gradParameters_D:norm())
+        return f,gradParameters_D
       end
 
-      gradParameters_D:zero() -- reset gradients
+      ----------------------------------------------------------------------
+      -- create closure to evaluate f(X) and df/dX of generator
+      local fevalG = function(x)
+        collectgarbage()
+        if x ~= parameters_G then -- get new parameters
+          parameters_G:copy(x)
+        end
 
-      --  forward pass
-      local outputs = model_D:forward(inputs)
-     -- err_F = criterion:forward(outputs:narrow(1, 1, opt.batchSize / 2), targets:narrow(1, 1, opt.batchSize / 2))
-     -- err_R = criterion:forward(outputs:narrow(1, (opt.batchSize / 2) + 1, opt.batchSize / 2), targets:narrow(1, (opt.batchSize / 2) + 1, opt.batchSize / 2))
-     err_R = criterion:forward(outputs:narrow(1, 1, opt.batchSize / 2), targets:narrow(1, 1, opt.batchSize / 2))
-     err_F = criterion:forward(outputs:narrow(1, (opt.batchSize / 2) + 1, opt.batchSize / 2), targets:narrow(1, (opt.batchSize / 2) + 1, opt.batchSize / 2))
+        gradParameters_G:zero() -- reset gradients
 
-    local margin = 0.3
-      sgdState_D.optimize = true
-      sgdState_G.optimize = true
-      if err_F < margin or err_R < margin then
-         sgdState_D.optimize = false
-      end
-      if err_F > (1.0-margin) or err_R > (1.0-margin) then
-         sgdState_G.optimize = false
-      end
-      if sgdState_G.optimize == false and sgdState_D.optimize == false then
-         sgdState_G.optimize = true
-         sgdState_D.optimize = true
-      end
+        -- forward pass
+        local samples = model_G:forward(noise_inputs)
+        local outputs = model_D:forward(samples)
+        local f = criterion:forward(outputs, targets)
+       io.write("G:",f, " G:", tostring(sgdState_G.optimize)," D:",tostring(sgdState_D.optimize)," ", sgdState_G.numUpdates, " ", sgdState_D.numUpdates , "\n")
+        io.flush()
 
-
-      --print(monA:size(), tarA:size())
-      io.write("v1_lfw| R:", err_R,"  F:", err_F, "  ")
-      local f = criterion:forward(outputs, targets)
-
-      -- backward pass
-      local df_do = criterion:backward(outputs, targets)
-      model_D:backward(inputs, df_do)
-
-      -- penalties (L1 and L2):
-      if opt.coefL1 ~= 0 or opt.coefL2 ~= 0 then
-        local norm,sign= torch.norm,torch.sign
-        -- Loss:
-        f = f + opt.coefL1 * norm(parameters_D,1)
-        f = f + opt.coefL2 * norm(parameters_D,2)^2/2
-        -- Gradients:
-        gradParameters_D:add( sign(parameters_D):mul(opt.coefL1) + parameters_D:clone():mul(opt.coefL2) )
-      end
-      -- update confusion (add 1 since targets are binary)
-      for i = 1,opt.batchSize do
-        local c
-        if outputs[i][1] > 0.5 then c = 2 else c = 1 end
-        confusion:add(c, targets[i]+1)
-      end
-      --print('grad D', gradParameters_D:norm())
-      return f,gradParameters_D
-    end
-
-    ----------------------------------------------------------------------
-    -- create closure to evaluate f(X) and df/dX of generator
-    local fevalG = function(x)
-      collectgarbage()
-      if x ~= parameters_G then -- get new parameters
-        parameters_G:copy(x)
+        --  backward pass
+        local df_samples = criterion:backward(outputs, targets)
+        model_D:backward(samples, df_samples)
+        local df_do = model_D.modules[1].gradInput
+        model_G:backward(noise_inputs, df_do)
+        print('gradParameters_G', gradParameters_G:norm())
+        return f,gradParameters_G
       end
 
-      gradParameters_G:zero() -- reset gradients
+      ----------------------------------------------------------------------
+      -- (1) Update D network: maximize log(D(x)) + log(1 - D(G(z)))
+      -- Get half a minibatch of real, half fake
+      for k=1,opt.K do
+        -- (1.1) Real data
+        local k = 1
+        for i = t,math.min(t+dataBatchSize-1,dataset:size()[1]) do
+          local idx = math.random(dataset:size()[1])
+          local sample = dataset[idx]
+          inputs[k] = sample:clone()
+          k = k + 1
+        end
+        targets[{{1,dataBatchSize}}]:fill(1)
+        -- (1.2) Sampled data
+        noise_inputs:normal(0, 1)
+        local samples = model_G:forward(noise_inputs[{{dataBatchSize+1,opt.batchSize}}])
+        for i = 1, dataBatchSize do
+          inputs[k] = samples[i]:clone()
+          k = k + 1
+        end
+        targets[{{dataBatchSize+1,opt.batchSize}}]:fill(0)
 
-      -- forward pass
-      local samples = model_G:forward(noise_inputs)
-      local outputs = model_D:forward(samples)
-      local f = criterion:forward(outputs, targets)
-     io.write("G:",f, " G:", tostring(sgdState_G.optimize)," D:",tostring(sgdState_D.optimize)," ", sgdState_G.numUpdates, " ", sgdState_D.numUpdates , "\n")
-      io.flush()
+        rmsprop(fevalD, parameters_D, sgdState_D)
 
-      --  backward pass
-      local df_samples = criterion:backward(outputs, targets)
-      model_D:backward(samples, df_samples)
-      local df_do = model_D.modules[1].gradInput
-      model_G:backward(noise_inputs, df_do)
-      print('gradParameters_G', gradParameters_G:norm())
-      return f,gradParameters_G
-    end
+      end -- end for K
 
-    ----------------------------------------------------------------------
-    -- (1) Update D network: maximize log(D(x)) + log(1 - D(G(z)))
-    -- Get half a minibatch of real, half fake
-    for k=1,opt.K do
-      -- (1.1) Real data
-      local k = 1
-      for i = t,math.min(t+dataBatchSize-1,dataset:size()[1]) do
-        local idx = math.random(dataset:size()[1])
-        local sample = dataset[idx]
-        inputs[k] = sample:clone()
-        k = k + 1
-      end
-      targets[{{1,dataBatchSize}}]:fill(1)
-      -- (1.2) Sampled data
+      ----------------------------------------------------------------------
+      -- (2) Update G network: maximize log(D(G(z)))
       noise_inputs:normal(0, 1)
-      local samples = model_G:forward(noise_inputs[{{dataBatchSize+1,opt.batchSize}}])
-      for i = 1, dataBatchSize do
-        inputs[k] = samples[i]:clone()
-        k = k + 1
+      targets:fill(1)
+      rmsprop(fevalG, parameters_G, sgdState_G)
+
+      -- display progress
+      xlua.progress(t, dataset:size()[1])
+    end -- end for loop over dataset
+
+    -- time taken
+    time = sys.clock() - time
+    time = time / dataset:size()[1]
+    print("<trainer> time to learn 1 sample = " .. (time*1000) .. 'ms')
+
+    -- print confusion matrix
+    print(confusion)
+    trainLogger:add{['% mean class accuracy (train set)'] = confusion.totalValid * 100}
+    confusion:zero()
+
+    -- save/log current net
+    if epoch % opt.saveFreq == 0 then
+      local filename = paths.concat(opt.save, 'adversarial.net')
+      os.execute('mkdir -p ' .. sys.dirname(filename))
+      if paths.filep(filename) then
+        os.execute('mv ' .. filename .. ' ' .. filename .. '.old')
       end
-      targets[{{dataBatchSize+1,opt.batchSize}}]:fill(0)
-
-      rmsprop(fevalD, parameters_D, sgdState_D)
-
-    end -- end for K
-
-    ----------------------------------------------------------------------
-    -- (2) Update G network: maximize log(D(G(z)))
-    noise_inputs:normal(0, 1)
-    targets:fill(1)
-    rmsprop(fevalG, parameters_G, sgdState_G)
-
-    -- display progress
-    xlua.progress(t, dataset:size()[1])
-  end -- end for loop over dataset
-
-  -- time taken
-  time = sys.clock() - time
-  time = time / dataset:size()[1]
-  print("<trainer> time to learn 1 sample = " .. (time*1000) .. 'ms')
-
-  -- print confusion matrix
-  print(confusion)
-  trainLogger:add{['% mean class accuracy (train set)'] = confusion.totalValid * 100}
-  confusion:zero()
-
-  -- save/log current net
-  if epoch % opt.saveFreq == 0 then
-    local filename = paths.concat(opt.save, 'adversarial.net')
-    os.execute('mkdir -p ' .. sys.dirname(filename))
-    if paths.filep(filename) then
-      os.execute('mv ' .. filename .. ' ' .. filename .. '.old')
+      print('<trainer> saving network to '..filename)
+      torch.save(filename, {D = model_D, G = model_G, opt = opt})
     end
-    print('<trainer> saving network to '..filename)
-    torch.save(filename, {D = model_D, G = model_G, opt = opt})
-  end
 
-  -- next epoch
-  epoch = epoch + 1
+    -- next epoch
+    epoch = epoch + 1
+  end
 end
 
 -- test function
